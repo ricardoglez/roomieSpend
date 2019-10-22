96c2ada7bb09c78bf9404d812deb55ae3a80a6d2
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-22 17:35:25 -0500

    Add new routes to login and signin

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,8 +10,10 @@ import AppActions from '../../actions/AppActions';
 import API from '../../utils/API'; 
 import { AppContextProvider, AppContext } from '../../context/AppContext';
 import Home from '../Home';
+import Login from '../Login';
+import SignIn from '../SignIn';
+
 const App = () => {
-    
     return (
         <Router basename='/'>
                 <Switch>
@@ -25,6 +27,8 @@ const App = () => {
                         <Route path='/viewExpenses'  exact>
                             <div>View Expenses</div>
                         </Route>
+                        <Route path='/crear_cuenta' component={SignIn} exact/>
+                        <Route path='/ingresar' component={Login} exact/>
                         <Route path='/' component={Home} exact/>
                     </AppContextProvider>
                 </Switch>
