eccc15fcf7ddb5b62cfd67c486339b001e379a50
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-22 17:38:21 -0500

    Add new condition to render NavBAr if user is not logged

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,15 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, BottomNavigationAction, BottomNavigation } from '@material-ui/core';
+import { AppContext } from '../../context/AppContext';
 
 const useStyles= makeStyles({ 
     root: { flexGrow: 1},
 });
 
 const NavigationBar = () => {
+    const [state, dispatch] = useContext(AppContext)
     const classes = useStyles();
     const [ value, setValue ] = useState(0);
+
+    if( !state.isAuth ){
+        return (
+            <BottomNavigation
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                showLabels
+                className={classes.root}
+                >
+                    <BottomNavigationAction
+                        component={Link}
+                        to='/'
+                        label="Home"
+                    />
+                    <BottomNavigationAction
+                        component={Link}
+                        to='/ingresar'
+                        label="Ingresar"
+                    />
+                    <BottomNavigationAction
+                        component={Link}
+                        to='/crear_cuenta'
+                        label="Crear Cuenta"
+                    />
+            </BottomNavigation>
+        )
+    }
 
     return (
         <BottomNavigation
