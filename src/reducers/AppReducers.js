5ec9eb1b726c709664780ee8cfd196310ce3334b
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-23 17:39:30 -0500

    add reducer to handle redirectTo

--- src/reducers/AppReducers.js
+++ src/reducers/AppReducers.js
@@ -1,12 +1,7 @@
 import { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const AppReducer = ( action ) => {
-
-    const [ state, dispatch ] = useContext( AppContext );
-
-    console.log('App Reducer');
-    console.log(state,action);
+const AppReducer = ( state, action ) => {
     switch( action.type ){
         case 'updateMounted':
             return {...state, isMounted: action.payload.status }
@@ -16,9 +11,11 @@ const AppReducer = ( action ) => {
                 return { ...state, userData: action.payload.userData }
         case 'updateAuthState':
                 return { ...state, isAuth: action.payload.isAuth }
+        case 'redirectTo':
+                return { ...state, pathToRedirect: action.payload.pathToRedirect, redirect: action.payload.redirect }
         default: 
             return { ...state }
     }
-}
+};
 
 export default AppReducer;
