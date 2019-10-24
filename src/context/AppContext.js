de31bfa3de44ffab8e8ec8accf6e774b6d3e1f84
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-24 16:18:36 -0500

    Check user status in App Context

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,9 +1,11 @@
 import React, { 
     useReducer, 
     createContext, 
-    useEffect, 
+    useEffect,
+    useState,
     useContext 
 } from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import { Redirect } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
 import AppReducers from '../reducers/AppReducers';
@@ -11,9 +13,12 @@ import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AppActions from '../actions/AppActions';
 import API from '../utils/API';
+import { userModel } from '../utils/common';
+
 
 const initialState = {
     isMounted   : false,
+    isSubmitting: false,
     isAuth      : false,
     fingerprint : null,
     userData    : null,
@@ -32,7 +37,6 @@ const AppContext =  createContext( initialState );
 const AppContextProvider = ({ children }) => {
     const classes = useStyles();
     const [state, dispatch] = useReducer(AppReducers, initialState); 
-
     return (
         <AppContext.Provider value={ [state, dispatch] } >
             <div className={ classes.root }>
@@ -47,17 +51,35 @@ const AppContextProvider = ({ children }) => {
     )
 }
 
-
 const AppBody = ({ children }) => {
     let [state , dispatch] = useContext(AppContext);
     
+    useEffect( () => {
+        console.log('User State');
+        API.getUserData()
+        .then( response => {
+            console.log(response);
+            const userObj = new userModel( response.data.uid, response.data.displayName, response.data.lastLogin, response.data.refreshToken );
+            AppActions.updateUserData(dispatch, userObj);
+            AppActions.updateAuthState(dispatch, true);
+            AppActions.updateMounted(dispatch, true);
+        })
+        .catch( error => {
+            console.error(error);
+            AppActions.updateMounted(dispatch, true);
+        })
+    },[])
+
     if(state.redirect && state.pathToRedirect){
         console.log(state.redirect, state.pathToRedirect);
         AppActions.redirectTo( dispatch, false ,null);
         return <Redirect push to={{ pathname: state.pathToRedirect }}/>
     } 
-    else {
+    else if( state.isMounted && !state.isSubmitting ) {
         return (children)
+    }
+    else {
+        return (<LinearProgress color="secondary" variant="query" />)
     }
 }
 export { AppContext, AppContextProvider };
