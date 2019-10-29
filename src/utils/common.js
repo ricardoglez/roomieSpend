1d8966a40e59f68bad584b2c9cdcf214c5830d69
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-29 08:24:27 -0600

    add props to userModel

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,10 +1,12 @@
 export const emailError = 'Ingresa un email válido'; 
 export const passwordError = 'La contraseña debe de tener al menos 6 caracteres (Solamente números y letras)'; 
- export class userModel {
-      constructor(userId, name, lastLogin, refreshToken ){
-          this.userId       = userId;
-          this.name         = name;
-          this.lastLogin    =  lastLogin;
-          this.refreshToken = refreshToken;
+export class userModel {
+      constructor(userId, name, lastLogin, refreshToken, teamId, assignedPurchases, debt ){
+          this.userId                   = userId;
+          this.name                     = name;
+          this.lastLogin                = lastLogin;
+          this.refreshToken             = refreshToken;
+          this.debt                     = debt;
+          this.assignedPurchases        = assignedPurchases;
       } 
  };
