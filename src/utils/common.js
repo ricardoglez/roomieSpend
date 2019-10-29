9299dec44f572685a1ef00787862eba5abda8601
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-29 12:52:36 -0600

    minors modifications in model

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,9 +1,9 @@
 export const emailError = 'Ingresa un email válido'; 
 export const passwordError = 'La contraseña debe de tener al menos 6 caracteres (Solamente números y letras)'; 
 export class userModel {
-      constructor(userId, name, lastLogin, refreshToken, teamId, assignedPurchases, debt ){
+      constructor(userId, name, lastLogin, refreshToken, assignedPurchases, debt ){
           this.userId                   = userId;
-          this.name                     = name;
+          this.displayName              = name;
           this.lastLogin                = lastLogin;
           this.refreshToken             = refreshToken;
           this.debt                     = debt;
