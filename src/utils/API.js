76c1d9e804392a523dc6b5f4de159e50f66d20c6
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-29 12:52:57 -0600

    new way to handle signin and update user data

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,14 @@
 import {firestore, firestoreDB } from './firebaseUtils';
 import firebase from 'firebase';
+
+import {userModel} from './common';
+
 const API = {
     checkUsernameAvailability: async ( username ) => {
         console.log('is Username Available...');
         return new Promise( (res, rej) => {
             try{
-                const query = firestore.collection('users').where('name','==', username);
+                const query = firestore.collection('users').where('displayName','==', username);
                 query.get()
                 .then( querySnapshot => {
                     res({ success:true , isAvailable: querySnapshot.docs.length === 0 });
@@ -16,6 +19,11 @@ const API = {
                 rej({success: false, error:error, data:null})
             }
         } );    
+    },
+    updateUser:(updatedUser) => {
+        const user = firebase.auth().currentUser;
+        console.log('Modify data of user', updatedUser, user);
+        return user.updateProfile(updatedUser)
     },
     getUserData: () => {
         console.log('Get user Data');
@@ -64,9 +72,54 @@ const API = {
             }
         } );    
     },
+    fetchUsers: () => {
+        console.log('Fetch users...');
+        return new Promise( (res, rej) => {
+            try{
+                let users = [];
+                firestore.collection('users')
+                    .onSnapshot( ( snapshot) => {
+                    snapshot.forEach( doc => { 
+                        users.push( doc.data());
+                    });
+                    res( {success: true , data: users} );
+
+                } );
+            }
+            catch(error){
+                rej({success: false, error:error, data:null})
+            }
+        } );    
+    },
     signIn:( data ) => {
         console.log('SignIn this', data);
-        return firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+        return new Promise( (res, rej)=> {
+            firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+            .then( response => {
+                console.log(response);
+                const userObj = { debt: 0 , displayName:data.username, userId: response.user.uid, assignedPurchases: {} };
+                
+                response.user.updateProfile({ displayName: data.username })
+                    .then( () => {
+                        firestore.collection('users').doc(userObj.userId).set(userObj);
+                        res({
+                            success: true, 
+                            data:{ 
+                                user: userObj,
+                                updatedProfile: firebase.auth().currentUser
+                            }
+                        });
+                    })
+                    .catch( error => {
+                        console.error(error);
+                        rej({ error});
+                    });
+            })
+            .catch(error => {
+                console.error(error);
+                rej({ error});
+            })
+        } );
     },
     logIn:( data ) => {
         console.log('LogIn this', data);
