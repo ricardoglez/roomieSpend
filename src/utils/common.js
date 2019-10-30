cc4a5d7e9ad158a9e15f8d78b550b4db5c7b6354
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-30 15:32:13 -0600

    add teamId to userMOdel

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,11 +1,12 @@
 export const emailError = 'Ingresa un email válido'; 
 export const passwordError = 'La contraseña debe de tener al menos 6 caracteres (Solamente números y letras)'; 
 export class userModel {
-      constructor(userId, name, lastLogin, refreshToken, assignedPurchases, debt ){
+      constructor(userId, name, lastLogin, refreshToken, teamId, assignedPurchases, debt, ){
           this.userId                   = userId;
           this.displayName              = name;
           this.lastLogin                = lastLogin;
           this.refreshToken             = refreshToken;
+          this.teamId                   = teamId;
           this.debt                     = debt;
           this.assignedPurchases        = assignedPurchases;
       } 
