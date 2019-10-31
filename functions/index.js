8388553a1962428eabf0faec981bc6245e0b08ea
Ricardo Gonzalez <ricardogonzalezamora@gmail.com>
2019-10-31 17:13:49 -0600

    ADd functon to handle puchase id after add new one

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,15 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
+exports.addIdToPurchase = functions.firestore.document('purchase/{purchaseId}')
+    .onCreate((snap, context) => {  
+        const purchaseId = context.params.purchaseId;
+        const newPurchase = snap.data();
+        console.log('Add to purchaseId', purchaseId);
+        console.log('newPurchase', newPurchase);
+        newPurchase['purchaseId'] = purchaseId;
+        db.collection('purchase').doc(purchaseId).set(newPurchase);
+});
 
 // exports.addToUsersCollection = functions.auth.user().onCreate((user,context) => {  
 //     console.log('Add to UsersCollection', user);
